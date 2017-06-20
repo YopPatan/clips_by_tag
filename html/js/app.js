c49fe753f74a2db2cfd4647f7142b130b190c09a
Jorge Talloni <jorge.talloni@tvn.cl>
2017-06-20 17:27:14 -0400

    Nueva version de clip by tag.

--- html/js/app.js
+++ html/js/app.js
@@ -1,45 +1,69 @@
-angular.module('copaApp', [])
-    .controller('VideosCtrl', function($scope, $http, $timeout) {
+var app = angular.module('copaApp', []);
+
+app.controller('VideosCtrl', function($scope, $http, $timeout, $location, $filter) {
+
+    //console.log($location.absUrl());
+    var url = new URL($location.absUrl());
+    var video_id = url.searchParams.get("id");
+
+    $scope.nombre_json = document.getElementById("nombre_json").value;
+    $scope.partido_equipos = document.getElementById("partido_equipos").value;
+    $scope.partido_fecha = document.getElementById("partido_fecha").value;
+    //console.log(document.getElementById("nombre_json").value);
+
+    $scope.loadData = function() {
+        if ($scope.isLista) {
+            $http.get('../services/data/' + $scope.nombre_json).then(function(response) {
+                //$scope.isLista = true;
+                console.log(response);
+                $scope.video_list = response.data;
+                //$timeout($scope.loadData, 10000);
+            });
+        }
+        $timeout($scope.loadData, 60000);
+    }
+
+    $scope.showVideo = function(video) {
+        //console.log('entroooo');
+        $scope.currentVideo = video;
+        $scope.isLista = false;
+        $scope.isVideo = true;
+
+        ga('send', 'event', 'copa_confederaciones','play: ' + video.title, window.location.href);
+
+        var param = {
+            url: video.video_hls,
+            imagen: video.thumbnail,
+            autoPlay: 1
+        }
+        $timeout(play.bind(null, 'videoMS', param), 500);
+    }
+
+    $scope.hideVideo = function() {
         $scope.isVideo = false;
         $scope.isLista = true;
+    }
 
-        $scope.nombre_json = document.getElementById("nombre_json").value;
-        $scope.partido_equipos = document.getElementById("partido_equipos").value;
-        $scope.partido_fecha = document.getElementById("partido_fecha").value;
-        //console.log(document.getElementById("nombre_json").value);
+    if (video_id != null) {
+        $scope.isVideo = true;
+        $scope.isLista = false;
+        $scope.isShared = true;
 
-        $scope.loadData = function() {
-            if ($scope.isLista) {
-                $http.get('../services/data/' + $scope.nombre_json).then(function(response) {
-                    //$scope.isLista = true;
-                    console.log(response);
-                    $scope.video_list = response.data;
-                    //$timeout($scope.loadData, 10000);
-                });
+        $http.get('../services/data/' + $scope.nombre_json).then(function(response) {
+            var videos = $filter('filter')(response.data, {'id': '5948113940992704b7163e00'}, true);
+            if (videos.length > 0) {
+                $scope.showVideo(videos[0]);
             }
-            $timeout($scope.loadData, 60000);
-        }
 
-        $scope.showVideo = function(video) {
-            //console.log('entroooo');
-            $scope.currentVideo = video;
-            $scope.isLista = false;
-            $scope.isVideo = true;
-
-
-            var param = {
-                url: video.video_hls,
-                imagen: video.thumbnail,
-                autoPlay: 1
-            }
-            $timeout(play.bind(null, 'videoMS', param), 500);
-        }
-
-        $scope.hideVideo = function() {
-            $scope.isVideo = false;
-            $scope.isLista = true;
-        }
-
+            console.log($filter('filter')(response.data, {'id': '5948113940992704b7163e00'}, true));
+            console.log(response.data);
+        });
+    }
+    else {
+        $scope.isVideo = false;
+        $scope.isLista = true;
+        $scope.isShared = false;
         $scope.loadData();
+    }
 
-    });
+});
--- services/generator_json.py
+++ services/generator_json.py
@@ -1,52 +1,101 @@
 import io
 import json
 import urllib2
-json_media = json.load(urllib2.urlopen(""))
+import sys
 
+media_ids = []
+tags_permitidos = []
 json_new = {}
 json_all = []
 
-for video in json_media['data']:
-    title = video['title']
-    description = video['description']
-    
-    if video['custom'].get('Minuto'):
-        mam = video['custom']['Minuto']
-    else:
-        mam = 'S/I'
+try:
+    json_conf = json.load(urllib2.urlopen('http://estaticos.tvn.cl/skins/copa_confederaciones/js/config.json'))
+except Exception as inst:
+    print "error al leer archivo de config"
+    sys.exit()
 
-    if len(video['thumbnails']) > 0:
-        thumbnail = video['thumbnails'][0]['url']
-    else:
-        thumbnail = ''
+if (json_conf.get("media_id")):
+    media_ids = json_conf["media_id"]
+else:
+    sys.exit()
 
-    if len(video['meta']) > 0:
-        video_mp4 = video['meta'][0]['url']
-    else:
-        video_mp4 = ''
+if (json_conf.get("tags_enabled")):
+    tags_permitidos = json_conf["tags_enabled"]
 
-    if video['protocols'].get('hls'):
-        video_hls = video['protocols']['hls']
-    else:
-        video_hls = ''
+for media_id in media_ids:
 
-    video_element = {'title': title, 'description': description, 'mam': mam, 'thumbnail': thumbnail, 'video_hls': video_hls, 'video_mp4': video_mp4}
-    json_all.append(video_element)
+    try:
+        json_media = json.load(urllib2.urlopen('https://api.streammanager.co/api/media?token=f5c4d4c019297f434f10c7a7bd87fe30&category_id=' + media_id))
+    except Exception as inst:
+        print "error al leer archivo con videos"
+        sys.exit()
 
-    for tag in video['tags']:
-#        print tag
+    for video in json_media['data']:
+        try:
 
-        if not json_new.get(tag):
-            json_new[tag] = []
+            id = video['_id']
+            title = video['title']
+            description = video['description']
 
-        json_new[tag].append(video_element)
+            if video.get('slug'):
+                slug = video['slug']
+            else:
+                slug = ''
 
-with io.open('data/data_all.json', 'w', encoding='utf-8') as f:
-    f.write(json.dumps(json_all, ensure_ascii=False))
+            if video.get('custom'):
+                if video['custom'].get('Minuto'):
+                    mam = video['custom']['Minuto']
+                else:
+                    mam = ''
+            else:
+                mam = ''
 
-for key, data in json_new.iteritems():
-    with io.open('data/data_' + key + '.json', 'w', encoding='utf-8') as f:
-        f.write(json.dumps(data, ensure_ascii=False))
+            if len(video['thumbnails']) > 0:
+                thumbnail = video['thumbnails'][0]['url']
+            else:
+                thumbnail = ''
+
+            if len(video['meta']) > 0:
+                video_mp4 = video['meta'][0]['url']
+            else:
+                video_mp4 = ''
+
+            if video['protocols'].get('hls'):
+                video_hls = video['protocols']['hls']
+            else:
+                video_hls = ''
+
+            video_element = {
+                'id': id,
+                'slug': slug,
+                'title': title,
+                'description': description,
+                'mam': mam,
+                'thumbnail': thumbnail,
+                'video_hls': video_hls,
+                'video_mp4': video_mp4
+            }
+
+            json_all.append(video_element)
+
+            if video.get("tags"):
+
+                for tag in video['tags']:
+
+                    if not json_new.get(tag):
+                        json_new[tag] = []
+
+                    json_new[tag].append(video_element)
+        except Exception as inst:
+            print inst
+
+    with io.open('data/data_' + media_id + '_all.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(json_all, ensure_ascii=False))
+
+    for key, data in json_new.iteritems():
+        if key in tags_permitidos:
+            with io.open('data/data_' + media_id + '_' + key + '.json', 'w', encoding='utf-8') as f:
+                f.write(json.dumps(data, ensure_ascii=False))
 
 #print json_new
 
